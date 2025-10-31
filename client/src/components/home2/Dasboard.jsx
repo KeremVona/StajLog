3d3b0159c26ed6c4da9fbb7e8665940f40815c96
KeremVona <keremvona@gmail.com>
2025-10-31 10:56:48 +0300

    remaining logs work

--- client/src/components/home2/Dasboard.jsx
+++ client/src/components/home2/Dasboard.jsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { Edit2, Redo2 } from "lucide-react";
+import { useState, useEffect } from "react";
+
+const API_URL = "http://localhost:5000/api/content/user";
+
 const Dashboard = ({ username, logs }) => {
+  const [remainingLogs, setRemainingLogs] = useState();
+  const l = logs.length;
+  useEffect(() => {
+    const handleFetch = async () => {
+      const token = localStorage.getItem("token");
+      try {
+        const result = await axios.get(API_URL, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        const remainingLogs2 = result.data.work_days - logs.length;
+        setRemainingLogs(remainingLogs2);
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+
+    handleFetch();
+  }, []);
   return (
     <div className="space-y-6">
       <header>
@@ -14,12 +40,18 @@ const Dashboard = ({ username, logs }) => {
           </h2>
           <p className="text-2xl font-bold text-green-600">{logs.length}</p>
         </div>
-        <div className="bg-white p-4 rounded-lg shadow-md">
-          <h2 className="text-lg font-semibold text-gray-700">
-            Pending Orders
-          </h2>
-          <p className="text-2xl font-bold text-yellow-600">3</p>
-        </div>
+        {remainingLogs != 0 ? (
+          <div className="bg-white p-4 rounded-lg shadow-md">
+            <h2 className="text-lg font-semibold text-gray-700">
+              Kalan kayıtlar
+            </h2>
+            <p className="text-2xl font-bold text-yellow-600">
+              {remainingLogs}
+            </p>
+          </div>
+        ) : (
+          ""
+        )}
         <div className="bg-white p-4 rounded-lg shadow-md">
           <h2 className="text-lg font-semibold text-gray-700">Total Spent</h2>
           <p className="text-2xl font-bold text-blue-600">$500.00</p>
