3985c349f02b2afe7b3b0fab70fee589db1ea82a
KeremVona <keremvona@gmail.com>
2026-06-29 16:16:39 +0300

    backend - route work

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { type Express } from "express";
 import cors from "cors";
 import userRoutes from "#/routes/auth/authRoute";
+import internshipRoutes from "#/routes/internship/internshipRoute";
 const app: Express = express();
 
 const port = 5000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", userRoutes);
+app.use("/api/internship", internshipRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
