51d72192981c75f8c114f2dcf4cba5a6e4cea8ad
KeremVona <keremvona@gmail.com>
2026-07-16 21:24:58 +0300

    backend - bits and pieces

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express, { type Express } from "express";
 import cors from "cors";
 import userRoutes from "#/routes/auth/authRoute";
 import internshipRoutes from "#/routes/internship/internshipRoute";
+import logRoutes from "#/routes/log/logRoute";
 const app: Express = express();
 
 const port = 5000;
@@ -12,6 +13,7 @@ app.use(cors());
 
 app.use("/api/auth", userRoutes);
 app.use("/api/internship", internshipRoutes);
+app.use("/api/log", logRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
---
+++ server/src/routes/log/logRoute.ts
@@ -0,0 +1,19 @@
+import {
+  deleteLogHandler,
+  editLogHandler,
+  getLogHandler,
+  getLogsHandler,
+  makeLogHandler,
+} from "#/controllers/log/logController.js";
+import authorize from "#/middlewares/auth/authorization.js";
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", authorize, getLogsHandler);
+router.get("/:id", authorize, getLogHandler);
+router.post("/", authorize, makeLogHandler);
+router.put("/:id", authorize, editLogHandler as any);
+router.delete("/:id", authorize, deleteLogHandler);
+
+export default router;
